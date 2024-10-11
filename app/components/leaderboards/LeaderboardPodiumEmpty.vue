4d7da61d436e23ac909a66b84758df16c13e22ec
Mauricio Herrera Ortega <ronalli56@gmail.com>
2024-10-10 22:06:18 -0300

    fix: change wave scale, add bottom gradient shadow & empty podium component

---
+++ app/components/leaderboards/LeaderboardPodiumEmpty.vue
@@ -0,0 +1,24 @@
+<script setup
+	lang="ts">
+
+import LeaderboardScoreStatus
+	from '~/components/leaderboards/LeaderboardScoreStatus.vue'
+import { cn } from '~/lib/utils'
+interface LeaderboardPodiumEmptyProps {
+	top: number
+	boxClass ?: string
+}
+
+const props = defineProps<LeaderboardPodiumEmptyProps>()
+</script>
+
+<template>
+	<div class="flex z-10 flex-col gap-2">
+		<div :class="cn('flex flex-col bg-gray-900 rounded-t-2xl h-24 w-16 items-center pt-2', props.boxClass)">
+		</div>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
