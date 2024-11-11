60ad15c78271f529c1eb863cfe7457b2b6dc844c
Mauricio Herrera Ortega <ronalli56@gmail.com>
2024-11-11 16:55:28 -0300

    challenge: add page & component of day 61 'flight-ticket'

---
+++ app/components/flight-ticket/FlightTicketInfoTitle.vue
@@ -0,0 +1,25 @@
+<script setup
+	lang="ts">
+
+interface FlightTicketInfoTitleProps {
+	label: string
+	content: string
+}
+
+defineProps<FlightTicketInfoTitleProps>()
+</script>
+
+<template>
+	<div class="flex flex-col items-center">
+		<span class="text-cyan-700 font-bold">
+			{{ label }}
+		</span>
+		<span class="text-xs text-black/75 font-bold">
+			{{ content }}
+		</span>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
