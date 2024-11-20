ab0cedf8a0c636a36ceaa0282d1a2a762d4aee4d
Mauricio Herrera Ortega <ronalli56@gmail.com>
2024-11-20 12:54:30 -0300

    fix: add 'cookies-banner' transition animation

--- app/pages/cookies-banner/index.vue
+++ app/pages/cookies-banner/index.vue
@@ -5,8 +5,8 @@ import CookiesBannerCustomize
 import CookiesBannerInfo
 	from '~/components/cookies-banner/CookiesBannerInfo.vue'
 
-const customize = ref(false)
-const show = ref(true)
+const customize = ref( false )
+const show      = ref( true )
 
 const savePreferences = () => {
 	show.value = false
@@ -15,22 +15,51 @@ const savePreferences = () => {
 
 <template>
 	<div class="w-screen h-screen flex items-center justify-center bg-[#d8d8d8]">
-		<div v-if="show" class="relative w-[450px] h-fit bg-gray-100 rounded-2xl px-4 p-8 flex flex-col items-center justify-between shadow-lg">
-			<div class="absolute -top-[8%] left-1/2 -translate-x-1/2 flex items-center justify-center">
-				<Icon size="50" name="unjs-cookie-es"/>
+		<transition>
+			<div v-if="show"
+				class="relative w-[450px] h-fit bg-gray-100 rounded-2xl px-4 p-8 flex flex-col items-center justify-between shadow-lg">
+				<div class="absolute -top-[8%] left-1/2 -translate-x-1/2 flex items-center justify-center">
+					<Icon size="50"
+						name="unjs-cookie-es"/>
+				</div>
+				<transition mode="out-in"
+					name="slide-fade">
+					<cookies-banner-info v-if="!customize"
+						@customize="customize = true"
+						@accept="savePreferences"
+					></cookies-banner-info>
+					<cookies-banner-customize v-else
+						@save="savePreferences"
+						@back="customize = false"
+					></cookies-banner-customize>
+				</transition>
 			</div>
-			<cookies-banner-info v-if="!customize"
-				@customize="customize = true"
-				@accept="savePreferences"
-			></cookies-banner-info>
-			<cookies-banner-customize v-else
-				@save="savePreferences"
-				@back="customize = false"
-			></cookies-banner-customize>
-		</div>
+		</transition>
 	</div>
 </template>
 
 <style scoped>
+.v-enter-active,
+.v-leave-active {
+	transition: opacity 0.4s ease;
+}
 
+.v-enter-from,
+.v-leave-to {
+	opacity: 0;
+}
+
+.slide-fade-enter-active {
+	transition: all 0.2s ease-out;
+}
+
+.slide-fade-leave-active {
+	transition: all 0.4s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+	transform: translateX(-20px);
+	opacity: 0;
+}
 </style>
