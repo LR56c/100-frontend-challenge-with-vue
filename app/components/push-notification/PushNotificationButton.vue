7772780c73daff423be6ce1e87588c728bae039d
Mauricio Herrera Ortega <ronalli56@gmail.com>
2024-11-23 09:31:38 -0300

    challenge: add page & component of day 73 'push-notification'

---
+++ app/components/push-notification/PushNotificationButton.vue
@@ -0,0 +1,22 @@
+<script setup
+	lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cn } from '~/lib/utils'
+
+interface PushNotificationButtonProps {
+	label : string
+	class?: HTMLAttributes['class']
+}
+
+const props = defineProps<PushNotificationButtonProps>()
+</script>
+
+<template>
+<button :class="cn('py-1 px-4 font-semibold capitalize border border-gray-400 text-gray-400 rounded-2xl', props.class)">
+	{{ label }}
+</button>
+</template>
+
+<style scoped>
+
+</style>
