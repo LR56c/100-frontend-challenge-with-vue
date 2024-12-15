c3ee6b5d77d7dd55ddc7865247af356f91302436
Mauricio Herrera Ortega <ronalli56@gmail.com>
2024-12-15 12:26:52 -0300

    challenge: add page & component of day 96 'suscribe-card'

---
+++ app/components/subscribe-card/SubscribeCardInput.vue
@@ -0,0 +1,22 @@
+<script setup
+	lang="ts">
+
+</script>
+
+<template>
+	<div class="h-10 flex items-center text-zinc-400 gap-2 bg-[#463d3e] text-sm rounded">
+		<input type="text"
+			required
+			id="default-text"
+			placeholder="Enter your email address"
+			class="bg-transparent w-full text-white placeholder-text-zinc-400 ps-4 outline-none"/>
+		<button
+			class="bg-[#f2ab42] font-semibold text-lg h-full text-black py-1 px-6 rounded">
+			Subscribe
+		</button>
+	</div>
+</template>
+
+<style scoped>
+
+</style>
