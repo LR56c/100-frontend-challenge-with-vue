f3771f9ebf858ff97fa3eb5de657716cb111a59c
Mauricio Herrera Ortega <ronalli56@gmail.com>
2024-12-17 15:05:58 -0300

    challenge: progress on day 99 'columns-card'

---
+++ app/components/columns-card/ColumnsCard.vue
@@ -0,0 +1,22 @@
+<script setup
+	lang="ts">
+export interface ColumnsCardProps {
+	title: string
+	textColor : string
+	backgroundColor : string
+	icon : string
+	content : string
+}
+
+defineProps<ColumnsCardProps>()
+</script>
+
+<template>
+<div :class="[backgroundColor, 'flex flex-col p-4 gap-2']">
+	<span>{{ title }}</span>
+</div>
+</template>
+
+<style scoped>
+
+</style>
